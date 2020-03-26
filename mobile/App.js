44caa0be47d0a49268576e42270f46d8b7d39b3a
Marcos Guillermo <53832367+MarcosDaNight@users.noreply.github.com>
2020-03-26 16:34:02 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,17 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'
+
+
+import React from 'react';
+
+
+// text n tem sematica como html, n existe herança de estilos
+import Routes from './src/routes';
+
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
